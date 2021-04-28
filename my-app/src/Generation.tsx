80296770491c2ee738535c9ce76570c0bdd87434
log g <logg.com@gmail.com>
2021-04-28 14:22:28 +0800

    click to download

--- my-app/src/Generation.tsx
+++ my-app/src/Generation.tsx
@@ -27,44 +27,44 @@ export const Generation = observer(() => {
     (imgElement.onload = function () {
       const canvas: any = document.getElementById("canvas");
       const ctx = canvas && canvas.getContext("2d");
-      canvas.width = imgElement.width;      // set canvas size big enough for the image
+      canvas.width = imgElement.width; // set canvas size big enough for the image
       canvas.height = imgElement.height;
       ctx.drawImage(imgElement, 0, 0, 256, 256);
-      
-      const height =256
-      const width = 256
-      console.log("height", height)
-      console.log("width", width)
+
+      const height = 256;
+      const width = 256;
+      console.log("height", height);
+      console.log("width", width);
       const imgData = ctx.getImageData(0, 0, height, width).data;
-      console.log(imgData)
-      let data_w= []
-      const data = []
+      console.log(imgData);
+      let data_w = [];
+      const data = [];
       let step = 0;
 
       for (let i = 0; i <= imgData.length; i += 4) {
         if (step == width) {
-          data.push(JSON.parse(JSON.stringify(data_w)))
+          data.push(JSON.parse(JSON.stringify(data_w)));
           // console.log(data_w)
           step = 0;
-          data_w= []
+          data_w = [];
         }
         data_w.push([imgData[i], imgData[i + 1], imgData[i + 2]]);
         // console.log(data_w)
         ++step;
       }
-    //   let mat = cv.imread("canvas");
+      //   let mat = cv.imread("canvas");
 
-    //   console.log("mat", mat);
-    //   let data = Array.from(mat.data[0:mat.cols*mat.rows*4]);
-    //   console.log("data",data)
-    //   const newArr = [];
-    //   while (data.length) newArr.push(data.splice(0, 4));
+      //   console.log("mat", mat);
+      //   let data = Array.from(mat.data[0:mat.cols*mat.rows*4]);
+      //   console.log("data",data)
+      //   const newArr = [];
+      //   while (data.length) newArr.push(data.splice(0, 4));
 
       // for (let  i=0; i<mat.rows*4; ++i){
 
       //     data[i] = mat.data[i];
       // }
-      gen.sourceimg = data
+      gen.sourceimg = data;
       console.log("sourceimg", gen.sourceimg);
     });
 
@@ -126,78 +126,76 @@ export const Generation = observer(() => {
 
   return (
     <>
-    <Box p={2} alignContent="center">
-
-    <b>Generation with First Order Motion Model </b><br></br>
-      Record your video:
       <Box p={2} alignContent="center">
-        <NewVideoComponent />
-      </Box>
-      {/* Put your Video here
+        <b>Generation with First Order Motion Model </b>
+        <br></br>
+        Record your video:
+        <Box p={2} alignContent="center">
+          <NewVideoComponent />
+        </Box>
+        {/* Put your Video here
 
           <DropzoneAreaVideo /> */}
-
-      <Box p={2}>
-      <Card raised >
-          
+        <Box p={2}>
+          <Card raised>
+            <Box p={5}>
+              Put your Photo here:
+              <DropzoneAreaPhoto />
+              Preview:
+              <img id="imageSrc" alt="No image" width="256" />
+            </Box>
+            {/* <canvas id="canvasOutput" ></canvas> */}
+          </Card>
+        </Box>
         <Box p={5}>
-        Put your Photo here:
-          <DropzoneAreaPhoto />
-        Preview:
-        <img id="imageSrc" alt="No image" width="256" />
-        </Box>
-        {/* <canvas id="canvasOutput" ></canvas> */}
-      </Card>
-        </Box>
-      <Box p={5}>
-        {state.genloading && <CircularProgress />}
-        {state.photoFile && !state.genloading && (
-          <Button
-            variant="contained"
-            color="primary"
-            // onClick={async () => {
-            //     const imgFile = state.photoFile;
-            //     const img = new Image();
+          {state.genloading && <CircularProgress />}
+          {state.photoFile && !state.genloading && (
+            <Button
+              variant="contained"
+              color="primary"
+              // onClick={async () => {
+              //     const imgFile = state.photoFile;
+              //     const img = new Image();
 
-            //     img.onload = function () {
-            //         const canvas: any = document.getElementById("canvas");
-            //         const ctx = canvas.getContext("2d");
-            //         ctx.drawImage(img, 0, 0);
-            //         const imgData = ctx.getImageData(0, 0, 256, 256).data;
-            //         console.log("imgData", imgData);
-            //     };
-            //     img.src = URL.createObjectURL(imgFile);
+              //     img.onload = function () {
+              //         const canvas: any = document.getElementById("canvas");
+              //         const ctx = canvas.getContext("2d");
+              //         ctx.drawImage(img, 0, 0);
+              //         const imgData = ctx.getImageData(0, 0, 256, 256).data;
+              //         console.log("imgData", imgData);
+              //     };
+              //     img.src = URL.createObjectURL(imgFile);
 
-            //     // // state.videoFile;
-            //     // const imgbuff = await img!.arrayBuffer();
-            //     // console.log(imgbuff);
-            //     // imageToRgbaMatrix(imgbuff).then(console.log);
-            //     // console.log("imgData", imgData);
-            // }}
-            onClick={() => {
-              console.log(gen);
-              // set loading
-              state.genloading = true;
-              postData("http://localhost:8000/gen", gen).then((data) => {
-                // setSvmresult(parseInt(data[1]));
-                console.log(data);
-                state.generationlink = data;
-                state.genloading = false;
-              });
-            }}
-          >
-            Deepfake this video and Photo
-          </Button>
-        )}
-        {state.generationlink && (
-          <a href={"http://localhost:8000/" + state.generationlink}>
-            Click me to download
-          </a>
-        )}
-      </Box>
-        <canvas id="canvas" 
-        width="256" height="256"
-        ></canvas>
+              //     // // state.videoFile;
+              //     // const imgbuff = await img!.arrayBuffer();
+              //     // console.log(imgbuff);
+              //     // imageToRgbaMatrix(imgbuff).then(console.log);
+              //     // console.log("imgData", imgData);
+              // }}
+              onClick={() => {
+                console.log(gen);
+                // set loading
+                state.genloading = true;
+                postData("http://localhost:8000/gen", gen).then((data) => {
+                  // setSvmresult(parseInt(data[1]));
+                  console.log(data);
+                  state.generationlink = data;
+                  state.genloading = false;
+                });
+              }}
+            >
+              Deepfake this video and Photo
+            </Button>
+          )}
+          {state.generationlink && (
+            <Box p={1}>
+              <a href={"http://localhost:8000/" + state.generationlink}>
+                Click me to download
+              </a>
+            </Box>
+          )}
+        </Box>
+        <canvas id="canvas" width="256" height="256"></canvas>
       </Box>
     </>
   );
